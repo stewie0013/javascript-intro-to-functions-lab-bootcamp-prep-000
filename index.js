171c651c3307f4dfcdcc9c539537801d7b8ce8d3
noah.stewart9813@gmail.com <noah.stewart9813@gmail.com>
2018-08-06 17:18:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function sayHiToGrandma(string) {
   
   if (string.toUpperCase() === uppercase) {
     return "YES INDEED!"
-  } else if (string.toLowerCase === lowercase) {
+  } else if (string.toLowerCase() === lowercase) {
     return "I can't hear you!"
   } else if (string === "I love you, Grandma.") {
     return "I love you, too."
